a4e949dc8e7900945dd5fb40644c0c1c02f528c3
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-31 22:26:15 +0200

    Add cipher method

--- prac/cipher.rb
+++ prac/cipher.rb
@@ -1,4 +1,9 @@
 shifted = "barna".chars.map {|c| c.ord}
 
-ascii = shifted.map {|c| c+5}
+ascii = shifted.map {|c| c+5 }
 
+puts ascii.map {|c| c.chr}.join
+
+def caesar_cipher(str, num)
+  ascii = str.chars.map {|c| c.ord}
+  shift = ascii.map {}
