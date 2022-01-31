b1573570bb56f1ccb2dfb3c75db967f716592ac0
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-31 22:05:58 +0200

    add cipher

--- prac/cipher.rb
+++ prac/cipher.rb
@@ -1,9 +1,17 @@
 def caesar_cipher(string, i)
-  new word = ''
+  new_word = ''
   string.each_char do |i|
-    n.times do
-      if
+    i.to_i.times do
+      if (i == 'z')
+        i = 'a'
+        next
+      end
+      i.next!
+      new_word += i
+    end
+    puts new_word
+  end
   
 end
 
-caesar_cipher('hekk')
+caesar_cipher('hekk', 2)
