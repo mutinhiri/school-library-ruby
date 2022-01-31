7c7375fc33368436829c5d8c9b3c1a4169d042f7
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-31 22:12:31 +0200

    Initiate character mappping

--- prac/cipher.rb
+++ prac/cipher.rb
@@ -1,9 +1,1 @@
-def encrypt(msg, key)
-    msg.downcase.split("").each_with_index do |char, i|
-        next if msg[i] == " "
-        msg[i] = (msg[i].ord + key) > 122 ? (((msg[i].ord + key) % 123) + 97).chr : (msg[i].ord + key).chr
-    end
-    msg
-end
-
-encrypt('hello', 5)
+puts ascii = "barna".chars.map {|c| c.ord}
