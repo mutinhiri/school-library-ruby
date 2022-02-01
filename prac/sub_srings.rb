9bdced2330f3e5814a2c38cca1dafaf0c760e738
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 02:08:47 +0200

    Initialize method for class

--- prac/sub_srings.rb
+++ prac/sub_srings.rb
@@ -4,10 +4,14 @@ class Animal
     @name = name
     @number_of_jobs = number_of_jobs
   end
+
+  def speak
+    "zvakamira sei sei"
+  end
 end
 
 animal_1 = Animal.new("rex", 3)
 animal_2 = Animal.new("bob", 8)
 
-puts animal_1
+puts animal_1.speak
 puts animal_2
