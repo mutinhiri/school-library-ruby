4c4ec8714d057336affd758aaf982125d1c54927
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 18:54:51 +0200

    Initialize method for child class

--- prac/prac.rb
+++ prac/prac.rb
@@ -11,3 +11,15 @@ class Parent
     puts 'Parent altered'
   end
 end
+
+class Child < Parent
+  def override()
+    puts 'Child overeride'
+  end
+
+  def altered()
+    puts 'Child Before parent altered'
+    super()
+    puts 'child after  parent altered'
+  end 
+end
