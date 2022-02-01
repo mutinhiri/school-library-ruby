71369b52e1dc6796568ec4097ee5a12a9b119bd5
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 11:48:04 +0200

    Add class method

--- prac/dog.rb
+++ prac/dog.rb
@@ -1,1 +1,12 @@
 require './animal.rb'
+
+class Dog < Animal
+  def intilialize(color, name = 'Unknown')
+    super('dog', 6, name)
+    @color = color
+  end
+
+  def bring_a_stick
+    'here is your stick'
+  end
+end
