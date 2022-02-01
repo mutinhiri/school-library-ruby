48fda5a7a68cb06cc0fc9597455a7810d78ec880
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 14:30:17 +0200

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,13 +1,11 @@
-# frozen_string_literal: true
-
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age:, name: 'unknown', parent_permission: true); end
+  def initialize(age:, name: 'Unknown', parent_permission: true); end
 
   def can_use_services?
-    of_age? || @pqrent_permission
+    of_age? || @parent_permission
   end
 
   def of_age?
