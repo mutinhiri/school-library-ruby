c636e9ec35353e266b7e9704fb7526e8bb33604e
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 14:09:40 +0200

    Add changes to Student constructor

--- student.rb
+++ student.rb
@@ -4,5 +4,10 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(age:, name: 'Unknown', parent_permission: true, classroom: 'None')
-    
+    super(age: age, name: name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
 end
