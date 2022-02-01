0c8543c1d6b778620135fa8957ef3a2a7a381e9c
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 11:37:24 +0200

    Add changes

--- prac/animal.rb
+++ prac/animal.rb
@@ -1,32 +1,23 @@
 class Animal
-    def initialize(type, number_of_legs, name = "Unknown")
-    @id = Random.rand(1..1000)
-    @name = name
-    @number_of_legs = number_of_legs
-    @type = type
+  ...
+
+  def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name = value
   end
 end
-
- def id
-  @id
- end
-
- def type 
-  @type
- end
-
- def number_of_legs
-  @number_of_legs
- end
-
- def name
-  @name
- end
-
- def name = (value)
-  @name = value
- end
-end
-
-animal_1 = Animal.new("rex", 3)
-animal_2 = Animal.new("bob", 8)
