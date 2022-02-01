a0c9da742117654a6ceaf99398f8b6bef161fc66
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 14:00:09 +0200

    can use service method initialize

--- person.rb
+++ person.rb
@@ -3,4 +3,12 @@ class Person
   attr_reader :id
 
   def initialize(age:, name: 'unknown', parent_permission: true); end
+
+  def can_use_services?
+    of_age? || @pqrent_permission
+  end
+
+  def of_age?
+    @age >=18
+  end
 end
