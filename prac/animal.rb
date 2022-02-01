3c22681fd0e8d6ea859c919410cee906a46737b0
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 11:17:59 +0200

    Add encapsulation method

--- prac/animal.rb
+++ prac/animal.rb
@@ -1,14 +1,5 @@
 class Animal
-  def initialize(name, number_of_jobs)
-    @id = Random.rand(1..1000)
-    @name = name
-    @number_of_jobs = number_of_jobs
-    @type = type
-  end
-
-  def speak
-    "zvakamira sei sei"
-  end
+ 
 end
 
 animal_1 = Animal.new("rex", 3)
