ee07f91d6819735e9a7be60cf6c7b5e822d1bb15
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-01 11:20:22 +0200

    modify class attributes

--- prac/animal.rb
+++ prac/animal.rb
@@ -1,4 +1,23 @@
 class Animal
+ def id
+  @id
+ end
+
+ def type 
+  @type
+ end
+
+ def number_of_legs
+  @number_of_legs
+ end
+
+ def name
+  @name
+ end
+
+ def name = (value)
+  @name = value
+ end
  
 end
 
