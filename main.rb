88caf201ded7a90dae6698404341b90e8222f351
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-03 17:34:06 +0200

    Add changes

--- main.rb
+++ main.rb
@@ -6,34 +6,35 @@ require './rental'
 
 class App
   def initialize
-    @peope = []
+    @people = []
     @books = []
     @rentals = []
     @options = {
       '1' => 'List all books',
       '2' => 'List all people',
-      '3' => 'Create a person Teacher/ Student',
+      '3' => 'Create a person',
       '4' => 'Create a book',
       '5' => 'Create a rental',
       '6' => 'List all rentals for a given person',
-      '7' => 'Exit'}
+      '7' => 'Exit'
+    }
   end
 
   def run
-    puts 'Welcome to Bunbee School Library'
+    puts 'Welcome to School Library App!'
 
     loop do
       puts ''
-      puts 'Select option by number'
-      @options.each { |key, value| puts "#{key} #{value}"}
+      puts 'Please choose an option by eterin a number:'
+      @options.each { |key, value| puts "#{key}) #{value}" }
       option = gets.chomp
       break if option == '7'
 
-      menu_select option
+      menu_choice option
     end
   end
 
-  def menu_select(option)
+  def menu_choice(option)
     case option
     when '1'
       list_books
@@ -48,7 +49,7 @@ class App
     when '6'
       list_rentals
     else
-      puts 'Input a valid option'
+      puts 'Not a valid option'
     end
   end
 
@@ -56,9 +57,9 @@ class App
 
   def list_books
     if @books.empty?
-      puts 'There is no book available in the library'
+      puts 'There is no book registered in the library'
     else
-      @books.each{ |book| puts book }
+      @books.each { |book| puts book }
     end
   end
 
@@ -66,12 +67,12 @@ class App
     if @people.empty?
       puts 'There is no one registered in the library'
     else
-      @people.each { |person| puts person}
+      @people.each { |person| puts person }
     end
   end
 
   def create_person
-    print 'Press (1) to create a student or press (2) to create teacher? [input the number]:'
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     option = gets.chomp
     case option
     when '1'
@@ -86,33 +87,33 @@ class App
   end
 
   def create_student
-    print 'Age:'
+    print 'Age: '
     age = gets.chomp
-    print 'Name:'
+    print 'Name: '
     name = gets.chomp
-    print 'Has parent permission? [Y/N]:'
-    allowed = gets.chomp
-    @people.push(Student.new(name: name, age: age, parent_permission: translate_answer(allowed.downcase)))
+    print 'Has parent permission? [Y/N]: '
+    pp = gets.chomp
+    @people.push(Student.new(name: name, age: age, parent_permission: translate_answer(pp.downcase)))
   end
 
   def create_teacher
-    print 'Age'
+    print 'Age: '
     age = gets.chomp
-    print 'Name'
+    print 'Name: '
     name = gets.chomp
-    print 'Specialization'
+    print 'Specialization: '
     specialization = gets.chomp
     @people.push(Teacher.new(name: name, age: age, specialization: specialization))
   end
 
   def translate_answer(ans)
-    %[yes y].include?(ans)
+    %w[yes y].include?(ans)
   end
 
   def create_book
-    print 'Title'
+    print 'Title: '
     title = gets.chomp
-    print 'Author'
+    print 'Author: '
     author = gets.chomp
     @books.push(Book.new(title, author))
     puts 'Book created successfully'
@@ -120,21 +121,21 @@ class App
 
   def create_rental
     puts 'Select a book from the following list by number'
-    @books.each_with_index { |book, index| puts "#{index} #{book}" }
+    @books.each_with_index { |book, index| puts "#{index}) #{book}" }
     book_i = gets.chomp
     puts ''
-    puts 'Select a person from the following list by number'
+    puts 'Select a person from the following list by number (not id)'
     @people.each_with_index { |person, index| puts "#{index}) #{person}" }
     person_i = gets.chomp
     puts ''
-    print 'Date'
+    print 'Date: '
     date = gets.chomp
     @rentals.push(Rental.new(date, @books[book_i.to_i], @people[person_i.to_i]))
     puts 'Rental created successfully'
   end
 
   def list_rentals
-    puts 'Select a person from the following list'
+    puts 'Select a person from the following list by number (not id)'
     @people.each_with_index { |person, index| puts "#{index}) #{person}" }
     person_i = gets.chomp
     puts ''
