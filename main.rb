d4faf170990395b9dcb7689f8735601151da7d33
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-07 16:07:24 -0500

    Create separate file for Handle Person class

--- main.rb
+++ main.rb
@@ -4,40 +4,8 @@ require './classroom'
 require './book'
 require './rental'
 require './app'
+require './handle_person'
 
-class HandlePerson
-  def initialize
-    @people = []
-  end
-
-  def translate_answer(ans)
-    %w[yes y].include?(ans)
-  end
-
-  def create_student(age, name, parent_permission)
-    @people.push(Student.new(name:, age:, parent_permission: translate_answer(parent_permission.downcase)))
-  end
-
-  def create_teacher(age, name, specialization)
-    @people.push(Teacher.new(name:, age:, specialization:))
-  end
-
-  def display_people
-    if @people.empty?
-      puts 'no one is registered in the library'
-    else
-      @people.each_with_index { |person, index| puts "#{index} #{person}" }
-    end
-  end
-
-  def get_id_from_index(index)
-    @people[index].id
-  end
-
-  def get_person_from_index(index)
-    @people[index]
-  end
-end
 
 class HandleBooks
   def initialize
