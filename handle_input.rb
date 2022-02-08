51c0049649ed5e6aa6c98c116e775a92bc780d71
Anibal Amoroso <76854002+sj1978@users.noreply.github.com>
2022-02-08 12:31:06 -0300

    fix offenses

--- handle_input.rb
+++ handle_input.rb
@@ -31,8 +31,7 @@ class MenuChoice
   end
 
   def create_person
-    print 'Do you want to create a student (1)
-    or a teacher (2)? [Input the number]: '
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     option = gets.chomp
     print 'Age'
     age = gets.chomp
