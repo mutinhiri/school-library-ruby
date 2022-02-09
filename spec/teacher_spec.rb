224c469770d6c8154744decb27bb887dee12ed9d
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-09 23:53:56 +0200

    Check if name has property

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,1 +1,12 @@
 require './teacher'
+require './person'
+
+describe Teacher do 
+  context 'test class creation' do
+    name = 'Sibo'
+    age = '24'
+    specialization = 'Math'
+    teacher = Teacher.new(id: nil, name: name, age: age, specialization: specialization)
+    it 'check name' do
+      expect(teacher.name).to eq(name)
+    end
