31d35f11d5dfe20dea6c1679a2540a1558813643
Anibal Amoroso <76854002+sj1978@users.noreply.github.com>
2022-02-09 19:50:58 -0300

    update

--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ require 'json'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(id:, age:, name: 'Unknown', parent_permission: true, classroom: 'None')
+  def initialize(id, age, name: 'Unknown', parent_permission: true, classroom: 'None')
     super(id:, age:, name:, parent_permission:)
     @classroom = classroom
   end
