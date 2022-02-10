585ff7d8d991745a4d549dde1431756a05155c1f
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-10 02:12:39 -0500

    Fix existing Person tests

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -3,19 +3,19 @@ require './person'
 describe Person do
   context 'With valid input' do
     before :each do
-      @person = Person.new 'id01', 'age', 'name', 'parent_permission'
-
+      @person = Person.new(name:"name", age:22)
+    end
       describe '#new' do
         it 'should take 4 parameters and return a Person object' do
-          @person.should_be_an_instance_of Person
+          @person.should be_an_instance_of Person
         end
       end
 
       describe '#id' do
-        it 'returns the correct id' do
-          @person.id.should eql 'id01'
+        it 'returns a correct uuid' do
+          uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+          expect(uuid_regex.match?(@person.id.to_s.downcase)).to eq(true)
         end
       end
     end
-  end
 end
